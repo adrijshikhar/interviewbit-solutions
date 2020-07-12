cc2c611665b738b1de170f62246cb63f34126d6a
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-07-12 21:11:12 +0530

    Updates test.cpp

--- test.cpp
+++ test.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
+struct node
+{
+  int data;
+  node *link;
+};
+
 int main()
 {
-  int x = 30;
-  for (int i = 0; i < 30; i++)
-  {
-    if ((x & 1 << i) != 0)
-    {
-      cout << i << " ";
-    }
-  }
-
+  node *A = nullptr;
+  node *temp = new node();
+  temp->data = 3;
+  temp->link = nullptr;
+  cout << A;
+  A = temp;
+  cout << A->data;
   return 0;
 }
