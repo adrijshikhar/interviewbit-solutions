d7088faf31cd26afb0dd36366e332a3a39349b2c
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-07-13 20:34:24 +0530

    Adds reverse strings

---
+++ Stacks and Queues/ReverseString.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+string reverseString(string A)
+{
+  stack<char> s;
+  string ans;
+  int i = 0;
+  while (i < A.size())
+  {
+    s.push(A[i]);
+    i++;
+  }
+  i = 0;
+  while (i < A.size())
+  {
+    ans += s.top();
+    s.pop();
+    i++;
+  }
+  return ans;
+}
+
+int main()
+{
+  string v1 = "asdfasdf";
+  string v2 = reverseString(v1);
+  cout << v2;
+
+  return 0;
+}
