619a318646bfd1c4cb4efd12d5cb2aa6e4165c3f
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-07-28 18:02:49 +0530

    Adds longest common subsequence

---
+++ Dynamic Programming/LongestCommonSubsequence.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs(string A, string B)
+{
+  int row = A.size(), col = B.size();
+  vector<vector<int>> dp(row + 1, vector<int>(col + 1, 0));
+  for (int i = 0; i <= row; i++)
+  {
+    for (int j = 0; j <= col; j++)
+    {
+      if (i == 0 || j == 0)
+        dp[i][j] = 0;
+      else if (A[i - 1] == B[j - 1])
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      else
+        dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+
+  return dp[row][col];
+}
+
+int main()
+{
+  int v2 = lcs("wsdg", "addwdf");
+
+  cout << v2 << " ";
+
+  return 0;
+}
