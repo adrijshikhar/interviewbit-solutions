c1db5088acebd72908e817e6747b0983bc671b31
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-07-28 21:03:15 +0530

    Adds lps2

--- Dynamic Programming/LongestPalindromeSubsequence.cpp
+++ Dynamic Programming/LongestPalindromeSubsequence.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
-#define ll long long
+
 using namespace std;
+
+struct hash_pair
+{
+  template <class T1, class T2>
+  size_t operator()(const pair<T1, T2> &p) const
+  {
+    auto hash1 = hash<T1>{}(p.first);
+    auto hash2 = hash<T2>{}(p.second);
+    return hash1 ^ hash2;
+  }
+};
 
 int lps(string A)
 {
@@ -27,10 +38,33 @@ int lps(string A)
   return L[0][n - 1];
 }
 
+unordered_map<pair<int, int>, int, hash_pair> mp;
+
+int lps2(string A, int i, int j)
+{
+  if (i == j)
+    return 1;
+
+  if (A[i] == A[j] && i + 1 == j)
+    return 2;
+  if (A[i] == A[j])
+  {
+    if (mp.find(make_pair(i + 1, j - 1)) == mp.end())
+      return lps2(A, i + 1, j - 1) + 2;
+    return mp[make_pair(i + 1, j - 1)];
+  }
+  if (mp.find(make_pair(i, j - 1)) == mp.end())
+    mp[make_pair(i, j - 1)] = lps2(A, i, j - 1);
+  if (mp.find(make_pair(i, j - 1)) == mp.end())
+    mp[make_pair(i + 1, j)] = lps2(A, i + 1, j);
+
+  return max(mp[make_pair(i, j - 1)], mp[make_pair(i + 1, j)]);
+}
 
 int main()
 {
-  int v2 = lps("wsdg");
+  string v = "bedeeed";
+  int v2 = lps2(v, 0, v.size() - 1);
 
   cout << v2 << " ";
 
