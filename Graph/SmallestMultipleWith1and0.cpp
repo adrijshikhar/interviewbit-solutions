201398cd011b413cf786760fefdb1ac9877bfa68
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-07-31 00:46:35 +0530

    Adds second approach in smallest multiple with 0 and 1

--- Graph/SmallestMultipleWith1and0.cpp
+++ Graph/SmallestMultipleWith1and0.cpp
@@ -40,11 +40,62 @@ string multiple(int A)
   }
   return "";
 }
+ // approach 2
+string multiple2(int A)
+{
+  queue<int> q;
+  int rem = 1 % A;
+
+  q.push(rem);
+  vector<int> vis(A + 1, -1);
+  vis[rem] = 1;
+  vector<pair<int, char>> par(A + 1, {-1, '1'});
+  while (!q.empty())
+  {
+    int tp = q.front();
+    q.pop();
+
+    if (tp == 0)
+    {
+      string s = "";
+
+      s += par[0].second;
+      int p = par[0].first;
+      while (p != -1)
+      {
+        s += par[p].second;
+        p = par[p].first;
+      }
+
+      reverse(s.begin(), s.end());
+
+      return s;
+    }
+
+    int a1 = (tp * 10) % A;
+    int a2 = ((tp * 10) % A + 1) % A;
+    if (vis[a1] == -1)
+    {
+
+      q.push(a1);
+      vis[a1] = 1;
+      par[a1] = {tp, '0'};
+    }
+
+    if (vis[a2] == -1)
+    {
+      q.push(a2);
+      vis[a2] = 1;
+      par[a2] = {tp, '1'};
+    }
+  }
+  return "";
+}
 
 int main()
 {
-  int v1 = 525;
-  string v2 = multiple(v1);
+  int v1 = 2;
+  string v2 = multiple2(v1);
   cout << v2;
 
   return 0;
