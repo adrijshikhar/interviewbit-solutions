677514f856bd8d03bfbc8c64b453d9833a47ac95
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-11 12:02:31 +0530

    Fixes knapsack 0 1 prob

--- Arrays/RotateMatrix.cpp
+++ Arrays/RotateMatrix.cpp
@@ -2,36 +2,26 @@
 
 using namespace std;
 
-void rotate(vector<vector<int>> &A)
-{
+void rotate(vector<vector<int>> &A) {
   vector<vector<int>> B = A;
   int n = A.size();
-  for (int i = 0; i < B.size(); i++)
-  {
-    for (int j = 0; j < B[i].size(); j++)
-    {
+  for (int i = 0; i < B.size(); i++) {
+    for (int j = 0; j < B[i].size(); j++) {
       B[j][n - 1 - i] = A[i][j];
     }
   }
   A = B;
 }
 
-int main()
-{
-  vector<vector<int>> v1{{
-                             1,
-                             2,
-                         },
-                         {
-                             4,
-                             5,
-                         }};
+int main() {
+  vector<vector<int>> v1{
+      {1, 2},
+      {4, 5},
+  };
 
   rotate(v1);
-  for (int i = 0; i < v1.size(); i++)
-  {
-    for (int j = 0; j < v1[i].size(); j++)
-    {
+  for (int i = 0; i < v1.size(); i++) {
+    for (int j = 0; j < v1[i].size(); j++) {
       cout << v1[i][j] << " ";
     }
     cout << endl;
--- Dynamic Programming/Knapsnack.cpp
+++ Dynamic Programming/Knapsnack.cpp
@@ -2,35 +2,27 @@
 
 using namespace std;
 
-vector<int> maximiseProfit(vector<int> &p, vector<int> &w, int m) {
-
+int maximiseProfit(vector<int> &p, vector<int> &w, int m) {
   int n = w.size();
   vector<int> ans(n, 0);
   vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-  for (int i = 1; i < n + 1; i++) {
-    for (int j = 1; j < m + 1; j++) {
+  for (int i = 1; i < n + 1; i++)
+    for (int j = 1; j < m + 1; j++)
       if (w[i - 1] > j)
         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
       else
         dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i - 1]] + p[i - 1]);
-    }
-  }
-  for (int i = 0; i < n + 1; i++) {
-    for (int j = 0; j < m + 1; j++)
-      cout << dp[i][j] << " ";
-    cout << endl;
-  }
-  return ans;
+
+  return dp[n][m];
 }
 
 int main() {
-  vector<int> p{1, 2, 5, 6};
-  vector<int> w{2, 3, 4, 5};
-  int m = 8;
-  vector<int> v2 = maximiseProfit(p, w, m);
-  for (int i = 0; i < v2.size(); i++)
-    cout << v2[i];
+  vector<int> p{60, 100, 120};
+  vector<int> w{10, 20, 30};
+  int m = 50;
+  int v2 = maximiseProfit(p, w, m);
+  cout << v2;
 
   return 0;
 }
